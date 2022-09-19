843a0a4deca499097a9dfdcd77dce7b625f8be8f
sahib2442 <kadersahib1234@gmail.com>
2022-09-19 08:16:40 +0530

    [sahib] Add: Insertdeta

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace BinarySearchTree
+{
+    class BinarySearchTree<T> where T : IComparable
+    {
+        int count =0;
+        bool result = false;
+        Node<T> Root;
+        Node<T> Current;
+        public void InsertData(T data)
+        {
+            if (Root == null)
+            {
+                this.Root = new Node<T>(data);
+                this.Current = Root;
+                count++;
+                return;
+            }
+            if (this.Current.data.CompareTo(data) > 0)
+            {
+                if (this.Current.leftNode == null)
+                {
+                    this.Current.leftNode = new Node<T>(data);
+                    this.Current = Root;
+                    count++;
+                }
+                else
+                {
+                    this.Current = this.Current.leftNode;
+                    InsertData(data);
+                }
+            }
+            else
+            {
+                if (this.Current.rightNode == null)
+                {
+                    this.Current.rightNode = new Node<T>(data);
+                    this.Current = Root;
+                    count++;
+                }
+                else
+                {
+                    this.Current = this.Current.rightNode;
+                    InsertData(data);
+                }
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,18 @@
+﻿using BinarySearchTree;
+
+class progrm
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Binary Search Tree.\n");
+        BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+        binarySearchTree.InsertData(56);
+        binarySearchTree.InsertData(30);
+        binarySearchTree.InsertData(70);
+        binarySearchTree.Display(binarySearchTree.GetRoot());
+        Console.WriteLine("\n");
+        Console.WriteLine("Count of element in binary search:" + binarySearchTree.GetSize());
+        bool result = binarySearchTree.SearchTree(56, binarySearchTree.GetRoot());
+        Console.WriteLine(result);
+    }
+}
